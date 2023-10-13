c0350c1a3a87aa0cdad1eae2de7480faacee35d0
Ritesh Pandey <riteshpandey72060@gmail.com>
2023-10-13 16:55:12 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import '../styles/App.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+/** import components */
+import Main from './Main.js';
+import Quiz from './Quiz.js';
+import Result from './Result.js';
+import { CheckUserExist } from '../helper/helper.js';
+
+
+/** react routes */
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Main></Main>
+  },
+  {
+    path : '/quiz',
+    element : <CheckUserExist><Quiz /></CheckUserExist>
+  },
+  {
+    path : '/result',
+    element : <CheckUserExist><Result /></CheckUserExist>
+  },
+])
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
